9ffbbb9cd2f303cebbc3496886f4113877557b0f
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2020-12-11 13:43:10 +0000

    Can now vary square length

--- src/program.m
+++ src/program.m
@@ -4,11 +4,12 @@ opengl('save', 'hardware');
 
 % Variables
 S = 1;			% Scaling factor
-N = 10000000;	% Number of squares
-NoP = 10;		% Number of planks
+N = 5;          % Number of squares
+NoP = 1000000;		% Number of planks
 D = S/NoP;		% Distance between planks
 SL = D/4;		% Length of squares
-
+cla('reset');
+axis([0 1.0 0 1.0]);
 % Plot the floor
 for i = 0:D:S
 	xline(i, '-', 'LineWidth', 2)
@@ -24,16 +25,16 @@ xcr = [...
 	SL * cosd(sq_angles) - SL * sind(sq_angles) + xc;...
 	-SL * cosd(sq_angles) - SL * sind(sq_angles) + xc;...
 	-SL * cosd(sq_angles) + SL * sind(sq_angles) + xc;...
-];
+]
 ycr = [...
 	-SL * sind(sq_angles) + SL * cosd(sq_angles) + yc;...
 	-SL * sind(sq_angles) - SL * cosd(sq_angles) + yc;...
 	SL * sind(sq_angles) - SL * cosd(sq_angles) + yc;...
 	SL * sind(sq_angles) + SL * cosd(sq_angles) + yc;...
-];
+]
 
 % Plot all the things
-patch(xcr, ycr, 'red');
+%patch(xcr, ycr, 'red');
 
 % Calculate Pi
 n = 0;
